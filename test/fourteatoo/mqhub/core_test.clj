b9e7e31cf6938d7e4241cee52d83c9a8be6f2257
Walter Pelissero <walter@pelissero.de>
2025-07-10 17:27:13 +0200

    Added fake unit test

---
+++ test/fourteatoo/mqhub/core_test.clj
@@ -0,0 +1,7 @@
+(ns fourteatoo.mqhub.core-test
+  (:require [clojure.test :refer :all]
+            [fourteatoo.mqhub.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I do nothing."
+    (is (= 0 0))))
