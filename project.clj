45d209ac4602dc3979bcacb8341bf9e7cdc39e52
Walter Pelissero <walter@pelissero.de>
2025-09-03 10:19:19 +0200

    Remove unused dependency

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.12.1"]
-                 [org.clojure/core.memoize "1.1.266"]
                  [cheshire "6.0.0"]
                  [org.clojure/tools.logging "1.3.0"]
                  [spootnik/unilog "0.7.32"]
