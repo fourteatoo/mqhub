a1d6cd2dfa1302b0605ff5afebf58bafb5da9f20
Walter Pelissero <walter@pelissero.de>
2025-10-22 13:39:39 +0200

    Upgrade ancient artifacts

--- project.clj
+++ project.clj
@@ -7,19 +7,19 @@
                  [cheshire "6.1.0"]
                  [org.clojure/tools.logging "1.3.0"]
                  [spootnik/unilog "0.7.32"]
-                 [org.clojure/tools.cli "1.1.230"]
+                 [org.clojure/tools.cli "1.2.245"]
                  [diehard "0.12.0"]
                  [clojure.java-time "1.4.3"]
-                 [cprop "0.1.20"]
+                 [cprop "0.1.21"]
                  [camel-snake-kebab "0.4.3"]
                  [clojurewerkz/machine_head "1.0.0"]
                  [clojurewerkz/quartzite "2.2.0"]
                  [com.draines/postal "2.0.5"]
                  [io.github.fourteatoo/clj-evohome "1.1.0"]
                  [io.github.fourteatoo/clj-blink "1.0.0"]
-                 [nrepl "1.4.0"]
+                 [nrepl "1.5.1"]
                  [mount "0.1.23"]
-                 [com.github.steffan-westcott/clj-otel-api "0.2.9"]]
+                 [com.github.steffan-westcott/clj-otel-api "0.2.10"]]
   :main ^:skip-aot fourteatoo.mqhub.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
