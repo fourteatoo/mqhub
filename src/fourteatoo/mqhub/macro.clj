a569410b1fc4054918a08d599b6cd97ffc27fe58
Walter Pelissero <walter@pelissero.de>
2026-02-05 09:30:41 +0100

    Parse topic as a template in macro.clj

--- src/fourteatoo/mqhub/macro.clj
+++ src/fourteatoo/mqhub/macro.clj
@@ -5,6 +5,6 @@
 
 (defn make-topic-listener [configuration]
   (fn [topic data]
-    (let [topic (mqtt/parse-topic topic [nil :name :rest])]
+    (let [topic (mqtt/parse-topic topic (:topic configuration))]
       (act/execute-actions (:actions configuration)
                            topic data))))
