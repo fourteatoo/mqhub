e44cb18feeb531607b0f460665dfd18ffe632f59
Walter Pelissero <walter@pelissero.de>
2026-02-05 09:28:23 +0100

    Refactor geo.clj to allow for clojure code instead of actions

--- src/fourteatoo/mqhub/geo.clj
+++ src/fourteatoo/mqhub/geo.clj
@@ -9,30 +9,51 @@
             [camel-snake-kebab.core :as csk]))
 
 
+(defn- execute-actions [actions topic data])
+
 (defmulti ^:private process-event (fn [_ data _] (:type data)))
 
 (defmethod process-event "transition"
   [topic data configuration]
   (when-let [events (get (:areas configuration) (:desc data))]
-    (act/execute-actions (get events (keyword (:event data)))
+    ((get events (keyword (:event data))) topic data)
+    #_(act/execute-actions (get events (keyword (:event data)))
                          topic data)))
 
 (defmethod process-event "location"
   [topic data configuration]
   (let [regions (set (:inregions data))]
-    (-> (mapcat (fn [[name events]]
+    (->> (mapcat (fn [[name events]]
                   ((if (regions name) :enter :leave) events))
                 (:areas configuration))
-        (act/execute-actions topic data))))
+         (run! #(% topic data)))))
 
 (defmethod process-event :default
   [topic data configuration]
   (log/debug "ignored event" {:topic topic :data data}))
 
+(defn- actions->fn [actions]
+  (if (and (vector? actions)
+           (map? (first actions)))
+    (fn [topic data]
+      (act/execute-actions actions topic data))
+    (act/make-code-fn '[ctx topic data] actions)))
+
+(defn- normalize-configuration [configuration]
+  (update configuration :areas update-vals #(update-vals % actions->fn)))
+
+(comment
+  (normalize-configuration {:foo 1
+                            :bar 2
+                            :areas {"home" {:enter '(do data)
+                                            :leave '(do topic)}
+                                    "office" {:enter [{:type :evo-home}]
+                                              :leave [{:type :evo-home}]}}}))
+
 (defn make-topic-listener [configuration]
   (fn [topic data]
     (let [data (json/parse-string data csk/->kebab-case-keyword)
-          topic (mqtt/parse-topic topic [:app :user :device :rest])]
+          topic (mqtt/parse-topic topic (:topic configuration))]
       (process-event topic data configuration))))
 
 #_ ((make-topic-listener {}) "topic" {:data 1})
