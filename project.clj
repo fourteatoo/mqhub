3fba041c1506ab1cbb7a9ec56425217a6efbe8ed
Walter Pelissero <walter@pelissero.de>
2026-03-31 16:26:08 +0200

    Simplify project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject io.github.fourteatoo/mqhub "0.3.0-SNAPSHOT"
-  :description "A simple(r) Home IoT Hub based on MQTT written in Clojure"
+  :description "A simple(r) Home IoT Hub based on MQTT"
   :url "http://github.com/fourteatoo/mqhub"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
